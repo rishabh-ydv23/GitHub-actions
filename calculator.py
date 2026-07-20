fef959fbaac5ac1a183086ce1d3eaad50aea4d23
Rishabh <rishabhyadavunnao@gmail.com>
2026-07-20 12:30:18 +0530

    Update

--- calculator.py
+++ calculator.py
@@ -7,9 +7,10 @@ def subtract(a, b):
 def multiply(a, b):
     return a * b
 
-num1 = int(input("Enter a number: "))
-num2 = int(input("Enter another number: "))
+if __name__ == "__main__":
+    num1 = int(input("Enter a number: "))
+    num2 = int(input("Enter another number: "))
 
-print(add(num1, num2))
-print(subtract(num1, num2))
-print(multiply(num1, num2))
+    print(add(num1, num2))
+    print(subtract(num1, num2))
+    print(multiply(num1, num2))
